921067a402f5d7ecfe26ae1315cf0726ca4bbefd
Nixonfernandes-18 <nixonfernandes18@gmail.com>
2025-04-03 10:06:18 +0530

    added backend

--- src/components/backend/server.js
+++ src/components/backend/server.js
@@ -14,8 +14,8 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 // const MONGO_URI = process.env.MONGO_URI;
-const JWT_SECRET = process.env.JWT_SECRET;
-const apiKey = process.env.HUGGINGFACE_API_KEY;
+// const JWT_SECRET = process.env.JWT_SECRET;
+// const apiKey = process.env.HUGGINGFACE_API_KEY;
 
 mongoose.connect('mongodb://localhost:27017/hack-well')
 const db = mongoose.connection;
